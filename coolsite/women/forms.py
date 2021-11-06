eadb5662a0798de5db70f732f64355d997c53e03
daron035 <kamil249@mail.ru>
2021-11-07 03:53:36 +0500

    улучшены функция представления, формы, теперь они связаны с моделью и нет повтора кода, в форме добавлен валидатор

--- coolsite/women/forms.py
+++ coolsite/women/forms.py
@@ -1,9 +1,22 @@
 from django import forms
 from .models import *
+from django.core.exceptions import ValidationError
 
-class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, label="Заголовок") # widget=forms.TimeInput(attrs={'class': 'form-input'}) можно задать для каждого поля
-    slug = forms.SlugField(max_length=255, label="URL")
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}), label="Текст")
-    is_published = forms.BooleanField(label="Публикация", required=False, initial=True)
-    cat = forms.ModelChoiceField(queryset=Category.objects.all(), label="Категория", empty_label="Категория не выбрана") # позволяет выбор единственного объекта модели
+class AddPostForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['cat'].empty_label = "Категория не выбрана"
+    class Meta:
+        model = Women
+        fields = ['title', 'slug', 'content', 'photo', 'is_published', 'cat'] #'__all__'
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'cols': 60, 'rows': 10}),
+        }
+    
+    def clean_title(self): # валидация поля title, которую мы прописываем сами
+        title = self.cleaned_data['title'] # если не прописывать валидаторы, они будут проверяться в классе модели
+        if len(title) > 200:
+            raise ValidationError('Длина превышает 200 символов')
+        
+        return title # также можно прописать валидатор для любого другого поля
--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -6,7 +6,7 @@ class Women(models.Model):
     title = models.CharField(max_length=255, verbose_name="Заголовок")
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
     content = models.TextField(blank=True, verbose_name="Текст статьи")
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True, verbose_name="Фото")
     time_create = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     time_update = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
     is_published = models.BooleanField(default=True, verbose_name="Публикация")
